577eac553cd37069835112704eac6502ddebce03
DuyDuc94 <117431803+DuyDucLH@users.noreply.github.com>
2023-07-05 17:54:46 +0700

    delete

--- Form/SQL Statement/insertAccount.sql
+++
@@ -1,8 +0,0 @@
-use PRJ_ShoppingOnlineWebsite
-insert into User_Account(username, password, role) values('admin', 'admin', 1)
-insert into User_Account values('user1', 'user1', 0, 1, '0943218394', 'chacaidoseve@gmail.com')
-insert into User_Account(Username, Password) values('user2', 'user2')
-insert into User_Account(Username, Password) values('user3', 'user3')
-
-select * from User_Account
-
--- Form/SQL Statement/insertOrderStatus.sql
+++
@@ -1,9 +0,0 @@
-use PRJ_ShoppingOnlineWebsite
-
-insert into Order_Status values ('Canceled')
-insert into Order_Status values ('Delivering')
-insert into Order_Status values ('Delivered ')
-
-select * from Order_Status
-
-
